f021ef2e4d4952d0fa8731ac2f8c3df1afdc6e77
dirkvanbergen <dirkvanbergen@gmail.com>
2019-06-10 14:05:45 +0200

    logging

--- modules/blacklist-routes.js
+++ modules/blacklist-routes.js
@@ -1,7 +1,11 @@
 module.exports = function () {
     this.nuxt.hook('generate:extendRoutes', async routes => {
+      console.log("---before---")
+      console.log(routes)
       const blackList = ['/content'];
       const routesToGenerate = routes.filter(page => !blackList.includes(page.route));
       routes.splice(0, routes.length, ...routesToGenerate);
+      console.log("---after---")
+      console.log(routes)
     });
   };
