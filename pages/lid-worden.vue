cc09daa78721fbef1a0267f5552ebfc639daaa85
dirkvanbergen <dirkvanbergen@gmail.com>
2019-06-12 12:16:17 +0200

    also do lidworden

--- pages/lid-worden.vue
+++ pages/lid-worden.vue
@@ -2,6 +2,17 @@
     <div>Lid worden!</div>
 </template>
 
+<script>
+export default {
+    
+    async fetch({store, params}) {
+        await store.dispatch('teams/getTeams');
+        await store.dispatch('pages/getPageHeaders');
+    }
+}
+</script>
+
+
 <style scoped>
 div {
     color: red;
