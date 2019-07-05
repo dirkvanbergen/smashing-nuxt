d5418a6ebc2c8400869da2c8057497d36c9539ca
Dirk van Bergen <dirk.van.bergen@waternet.nl>
2019-07-05 10:14:41 +0200

    nu goed om

--- plugins/contentful.js
+++ plugins/contentful.js
@@ -2,6 +2,6 @@ require("dotenv").config()
 const contentful = require("contentful")
 
 module.exports = contentful.createClient({
-  space: "6N9kZ1ncGM6uS9otrP0Hy6n5NXA8BmW5nFJCkzTmgWE",
-  accessToken: "crmkzp2rr7hq"
+  space: "crmkzp2rr7hq",
+  accessToken: "6N9kZ1ncGM6uS9otrP0Hy6n5NXA8BmW5nFJCkzTmgWE"
 })
