fde07ce1cf4d56e2c763dec0a0a78c3b10736334
Dirk van Bergen <dirk.van.bergen@waternet.nl>
2020-06-21 18:30:29 +0200

    fixed news

--- components/NewsArticle.vue
+++ components/NewsArticle.vue
@@ -7,9 +7,9 @@
         <div class="md-content" v-html="documentToHtmlString(post.fields.text)"></div>
         <div class="flex flex-row flex-wrap -mx-2" v-if="post.fields.attachments && post.fields.attachments.length > 0">
           <div class="news-attachment flex-grow-0 flex-shrink-0 w-1/2 xl:w-1/6 md:w-1/4 p-2" v-for="(attachment, attachmentIndex) in post.fields.attachments" :key="attachmentIndex">
-            <img class="cursor-pointer rounded" @click="clickImage" :src="attachment.fields.file.url" :alt="attachment.fields.title" v-if="attachment.fields.file.contentType.startsWith('image/')" />
-            <a class="block text-center h-full flex flex-col-reverse justify-between p-2 border rounded relative" v-else :href="attachment.fields.file.url" target="_blank">{{attachment.fields.title}}
-              <img src="@/static/images/ball.svg" class="self-center w-1/2 h-1/2 mt-2"></a>          
+            <img class="cursor-pointer rounded" @click="clickImage" :src="attachment.fields.file.url" :alt="attachment.fields.title" v-if="attachment.fields && attachment.fields.file.contentType.startsWith('image/')" />
+            <a class="block text-center h-full flex flex-col-reverse justify-between p-2 border rounded relative" v-else-if="attachment.fields" :href="attachment.fields.file.url" target="_blank">{{attachment.fields.title}}
+              <img src="@/static/images/ball.svg" class="self-center w-1/2 h-1/2 mt-2"></a>
           </div>
         </div>
     </div>
--- pages/teams/_slug.vue
+++ pages/teams/_slug.vue
@@ -1,20 +1,25 @@
 <template>
   <div class="flex flex-row flex-wrap items-start">
-    <ContentSideMenu class="order-2 md:order-1 mt-4 md:mt-0 w-full mx-4 md:mx-0 md:w-1/3 lg:w-1/4"/>
-    <div class="order-1 md:order-2 mx-4 md:mx-0 md:pl-8 md:w-2/3 lg:w-3/4">
-      <h1 class="text-2xl text-semibold mb-1 text-smashing">{{currentTeam.fields.title}}</h1>
-      <h3 class="text-xl text-semibold" v-if="currentTeam.fields.level">{{currentTeam.fields.level}}</h3>
-      <ul class="py-2" v-if="currentTeam.fields.players && currentTeam.fields.players.length > 0">
-        <li v-for="(player, index) in currentTeam.fields.players" :key="index">{{player}}</li>
-      </ul>
-      <div v-if="currentTeam.fields.trainer && currentTeam.fields.trainer === currentTeam.fields.coach">Trainer &amp; coach: {{currentTeam.fields.trainer}}</div>
-      <div v-else-if="currentTeam.fields.trainer">Trainer: {{currentTeam.fields.trainer}}</div>
-      <div v-if="currentTeam.fields.coach && currentTeam.fields.trainer !== currentTeam.fields.coach">Coach: {{currentTeam.fields.coach}}</div>
-      <div v-if="currentTeam.fields.trainingstijden">
-        <div>Trainingstijden:</div>
-        <div v-for="(t, index) in currentTeam.fields.trainingstijden" :key="index" class="ml-4">{{t}}</div>
+    <ContentSideMenu class="order-2 md:order-1 mt-4 md:mt-0 mx-4 md:mx-0 w-full md:w-1/3 lg:w-1/4"/>
+    <div class="order-1 md:order-2 mx-4 md:mx-0 w-full md:w-2/3 lg:w-3/4 flex flex-row flex-wrap items-start">
+      <div class="w-full lg:w-2/3 md:pl-8">
+        <h1 class="text-2xl text-semibold mb-1 text-smashing">{{currentTeam.fields.title}}</h1>
+        <h3 class="text-xl text-semibold" v-if="currentTeam.fields.level">{{currentTeam.fields.level}}</h3>
+        <ul class="py-2" v-if="currentTeam.fields.players && currentTeam.fields.players.length > 0">
+          <li v-for="(player, index) in currentTeam.fields.players" :key="index">{{player}}</li>
+        </ul>
+        <div v-if="currentTeam.fields.trainer && currentTeam.fields.trainer === currentTeam.fields.coach">Trainer &amp; coach: {{currentTeam.fields.trainer}}</div>
+        <div v-else-if="currentTeam.fields.trainer">Trainer: {{currentTeam.fields.trainer}}</div>
+        <div v-if="currentTeam.fields.coach && currentTeam.fields.trainer !== currentTeam.fields.coach">Coach: {{currentTeam.fields.coach}}</div>
+        <div v-if="currentTeam.fields.trainingstijden">
+          <div>Trainingstijden:</div>
+          <div v-for="(t, index) in currentTeam.fields.trainingstijden" :key="index" class="ml-4">{{t}}</div>
+        </div>
+        <div class="extra-text mt-2 md-content" v-html="documentToHtmlString(currentTeam.fields.extraText)"></div>
       </div>
-      <!-- <div class="extra-text mt-2 md-content" v-html="documentToHtmlString(currentTeam.fields.extraText)"></div> -->
+      <div class="photo w-full lg:w-1/3">
+        Fototje
+      </div>
     </div>
   </div>
 </template>
@@ -24,15 +29,17 @@ import { documentToHtmlString } from "@contentful/rich-text-html-renderer";
 import ContentSideMenu from "@/components/ContentSideMenu";
 export default {
   components: { ContentSideMenu },
+  methods: {
+    documentToHtmlString(doc) {
+      return documentToHtmlString(doc);
+    }
+  },
   computed: {
     currentTeam() {
       return this.$store.state.team.currentTeam;
     },
     isLoading() {
       return this.$store.state.team.isLoading;
-    },
-    documentToHtmlString(doc) {
-      return documentToHtmlString(doc);
     }
   },
   async fetch({ store, params }) {
