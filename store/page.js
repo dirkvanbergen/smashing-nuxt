df16aa5119f9863ec5dfd163836203611ba585cf
Dirk van Bergen <dirk.van.bergen@visionhealthcare.eu>
2021-05-22 12:40:10 +0200

    same page

--- store/page.js
+++ store/page.js
@@ -3,7 +3,8 @@ import client from "../plugins/contentful"
 export const state = () => ({
   currentPage: {},
   isLoading: true,
-  isNotFound: false
+  isNotFound: false,
+  pageType: ''
 })
 
 export const mutations = {
@@ -15,31 +16,13 @@ export const mutations = {
   },
   setNotFound(state, payload) {
     state.isNotFound = payload
+  },
+  setPageType(state, payload) {
+    state.pageType = payload
   }
 }
 
 export const actions = {
-  async getFirstPageFromParent({ commit }, { item }) {
-    commit("setLoading", true)
-    console.log("getFirstPageFromParent")
-    const response = await client.getEntries({
-      content_type: "page",
-      order: "fields.order"
-    })
-    let result = response.items.filter(
-      page =>
-        page.fields.parent.fields.slug === item
-    )
-    console.table(result)
-    if (result.length == 0) {
-      commit("setCurrentPage", null)
-      commit("setNotFound", true)
-    } else {
-      commit("setCurrentPage", result[0])
-      commit("setNotFound", false)
-    }
-    commit("setLoading", false)
-  },
   async getPageBySlug({ commit }, { item, slug }) {
     commit("setLoading", true)
     const response = await client.getEntries({
@@ -50,10 +33,26 @@ export const actions = {
       page =>
         page.fields.parent.fields.slug === item && page.fields.slug === slug
     )
-    if (result.length == 0) {
-      commit("setCurrentPage", null)
-      commit("setNotFound", true)
+    if (result.length == 0) {      
+      const teamResponse = await client.getEntries({
+        content_type: "team",
+        "fields.slug": slug
+      })
+      let teams = teamResponse.items.filter(
+        page =>
+          page.fields.parent.fields.slug === item && page.fields.slug === slug
+      )
+      if (teams.length == 0) {
+        commit("setPageType", "error")
+        commit("setCurrentPage", null)
+        commit("setNotFound", true)
+      } else {        
+        commit("setPageType", "team")
+        commit("setCurrentPage", teams[0])
+        commit("setNotFound", false)
+      }
     } else {
+      commit("setPageType", "content")
       commit("setCurrentPage", result[0])
       commit("setNotFound", false)
     }
