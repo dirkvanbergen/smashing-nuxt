ea359aef2fe56c3e9945af2cc1e863f34ab6c6cb
Dirk van Bergen <dirk.van.bergen@visionhealthcare.eu>
2021-09-28 15:59:28 +0200

    Add rendering of PDF

--- modules/render-options.js
+++ modules/render-options.js
@@ -7,9 +7,12 @@ export default {
       },
       ["embedded-asset-block"]: (node) => {
         var file = node.data.target.fields.file;
+        var title = node.data.target.fields.title;
         if (file.contentType.substr(0, 5) === 'image') {
-          var title = node.data.target.fields.title;
           return `<a href="https:${file.url}" title="${title}" target="_blank"><img alt="${title}" src="${file.url}" style="max-width:100%" /></a>`
+        }
+        if (file.contentType === 'application/pdf') {
+          return `<iframe src="${file.url}" width="100%" height="700px" style="border: 0;"></iframe><a href="https:${file.url}" title="${title}" target="_blank">Download ${title}</a>`
         }
       }
     }
