0e3e943b2e5e8e513d6ec9212d16725375618096
Dirk van Bergen <dirkvanbergen@gmail.com>
2024-08-31 16:01:02 +0200

    Update index.vue
    remove webshop

--- pages/index.vue
+++ pages/index.vue
@@ -5,16 +5,6 @@
         <NewsArticle v-for="(post, index) in news" :key="index"  :post="post" />
       <nuxt-link class="" :to="{name: 'archief'}">Ouder nieuws ...</nuxt-link>
       </div>
-      <div class="col-md-4 col-sm-12">
-      <a
-        href="https://erimashop.nl/smashing"
-        target="_blank"
-        class="webshop-button"
-        @click.native="menuClick"
-      ><img src="@/static/images/teaco.png" />Smashing '72 Webshop</a>
-      <br />
-      <iframe src="https://bannerbuilder.sponsorkliks.com/skinfo.php?&background-color=FFFFFF&text-color=304B61&header-background-color=304B61&header-text-color=FFFFFF&odd-row=FFFFFF&even-row=09494a&odd-row-text=09494a&even-row-text=ffffff&type=financial&club_id=11880&width=300&height=250" frameborder="0" width="300" height="250"></iframe>
-      </div>
     </div>
   </div>
 </template>
